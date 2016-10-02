0586d2c7d7939e4bde9aff9beef957ad12d68bde
Kevin Gumba <kevin.gumba@gmail.com>
2016-10-01 20:56:57 -0700

    adding skeleton files for StickyNotes. Implemented delayed save in controller.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sticky Notes
+
+Work in progress.
+Description: A windows application allowing users to type, instantly save texts, and retrieve archived texts.
---
+++ StickyNotesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StickyNotes
+{
+    class StickyNotesController
+    {
+        private int SAVE_DELAY = 6000;
+        private Thread delayedSaveThread;
+        private Object _delayedSaveLock = new Object();
+
+        public void triggerDelayedSave()
+        {
+            lock (_delayedSaveLock)
+            {
+                if (delayedSaveThread == null)
+                {
+                    delayedSaveThread = new Thread(this.doDelayedSave);
+                    delayedSaveThread.Start();
+                }
+            }
+            
+        }
+
+        private void doDelayedSave()
+        {
+            Thread.Sleep(SAVE_DELAY);
+            save();
+            lock (_delayedSaveLock)
+            {
+                delayedSaveThread = null;
+            }
+        }
+
+        private void save()
+        {
+
+        }
+    }    
+}
