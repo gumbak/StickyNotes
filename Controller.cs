85eaf6346e163a2a6faea0f37a40f99798f1ad89
Kevin Gumba <kevin.gumba@gmail.com>
2016-10-03 21:55:50 -0700

    implement 'get' functionality in database to grab data based on id and fieldName.

---
+++ Controller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StickyNotes
+{
+    class Controller
+    {
+        private Database _database;
+        private int SAVE_DELAY = 3000;
+        private Thread _delayedSaveThread;
+        private Object _delayedSaveLock = new Object();
+
+        public Controller()
+        {
+            _database = new StickyNotes.Database();
+        }
+
+        public void triggerDelayedSave()
+        {
+            lock (_delayedSaveLock)
+            {
+                if (_delayedSaveThread == null)
+                {
+                    _delayedSaveThread = new Thread(this.doDelayedSave);
+                    _delayedSaveThread.Start();
+                }
+            }
+            
+        }
+
+        private void doDelayedSave()
+        {
+            Thread.Sleep(SAVE_DELAY);
+            save();
+            lock (_delayedSaveLock)
+            {
+                _delayedSaveThread = null;
+            }
+        }
+
+        private void save()
+        {
+            // TODO: pass ID and content
+            _database.save("temp", "temp");
+        }
+
+        public async void getContent(string id)
+        {
+            string content = await _database.get(id, ConfigurationManager.AppSettings["DATABASE_FIELD_NAME_CONTENT"]);
+        }
+    }    
+}
--- Database.cs
+++ Database.cs
@@ -30,7 +30,20 @@ namespace StickyNotes
             };
 
             IMongoCollection<BsonDocument> collection = _database.GetCollection<BsonDocument>(COLLECTION_PATH);
-            collection.InsertOne(document);
+            collection.InsertOneAsync(document);
+        }
+
+        public async Task<string> get(string id, string fieldName)
+        {
+            IMongoCollection<BsonDocument> collection = _database.GetCollection<BsonDocument>(COLLECTION_PATH);
+            var filter = Builders<BsonDocument>.Filter.Eq("id", id);
+            var cursor = await collection.FindAsync(filter);
+            while (await cursor.MoveNextAsync())
+            {
+                var document = cursor.Current.First();
+                return document.GetValue(fieldName).AsString;
+            }
+            return null;
         }
     }
 }
